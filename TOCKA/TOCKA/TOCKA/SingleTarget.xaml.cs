6d8b3d31cff3fbdede2831ebbdd20aedba128aa4
Karolgorski <goralk2870@gmail.com>
2016-11-26 17:55:37 +0100

    Dodałem strony

---
+++ TOCKA/TOCKA/TOCKA/SingleTarget.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace TOCKA
+{
+    public partial class SingleTarget : ContentPage
+    {
+        public SingleTarget()
+        {
+            InitializeComponent();
+        }
+    }
+}
