754554f28b2aeade3f0be429a21edec1bde59f76
Karolgorski <goralk2870@gmail.com>
2016-11-26 20:05:45 +0100

    ekran logowania na stringach

--- TOCKA/TOCKA/TOCKA/App.cs
+++ TOCKA/TOCKA/TOCKA/App.cs
@@ -11,23 +11,9 @@ namespace TOCKA
     {
         public App()
         {
-            // The root page of your application
-            var content = new ContentPage
-            {
-                Title = "TOCKA",
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            HorizontalTextAlignment = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
 
-            MainPage = new NavigationPage(content);
+
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
--- TOCKA/TOCKA/TOCKA/MainPage.xaml.cs
+++ TOCKA/TOCKA/TOCKA/MainPage.xaml.cs
@@ -10,9 +10,33 @@ namespace TOCKA
 {
     public partial class MainPage : ContentPage
     {
+
         public MainPage()
         {
             InitializeComponent();
+
         }
+
+        private async void NavigateButton_OnClicked(object sender, EventArgs e)
+        {
+            string login;
+            string password;
+            login = LoginEntry.Text;
+            password = PasswordEntry.Text;
+
+            if (ifTrue(login, password))
+                await Navigation.PushAsync(new FrontPage());
+            else
+             await DisplayAlert("Fail", "Failed to log in. Check your Login information.", "OK");
+
+        }
+
+        private bool ifTrue(string login, string pass)
+        {
+            if ((login == "eh") && (pass == "eh"))
+            return true;
+            return false;
+        }
+
     }
 }
